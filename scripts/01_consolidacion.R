672e74d410701647000320273f8ab363c9a02355
Robert Macías <rsmacias@gmail.com>
2022-02-01 14:57:05 -0500

    Se agrega la transformación inicial de los datos

--- scripts/01_consolidacion.R
+++ scripts/01_consolidacion.R
@@ -36,7 +36,45 @@ for (archivo in data.base$files_path) {
   }
 }
 
-data.ifis %>% filter( tipo_ifi == "COOPERATIVAS") %>% view()
+# Transformación datos ---------------------------------------------------------
+
+if(exists("data.ifis")){
+  print("Transformacion General")
+  temporal <- data.ifis %>%
+    pivot_longer(
+      cols = starts_with("X"),
+      names_to = "fecha_fin_mes",
+      values_to = "valor",
+      values_drop_na = FALSE
+    ) %>% separate(
+      fecha_fin_mes, 
+      c("dia", "mes", "anio"), 
+      remove = FALSE, 
+      convert = TRUE
+    ) %>% mutate( 
+      dia = as.integer(str_sub(dia, -2)),
+      fecha_fin_mes = as.Date(str_sub(fecha_fin_mes, -10), format = "%d.%m.%Y")
+    ) %>% rename(
+      "codigo_ifi" = "COD..IFI",
+      "nombre_ifi" = "COD..INDICE",
+      "nombre_ratio_financiero" = "CUENTA",
+      "codigo_ratio_financiero" = "INDICE"
+    ) %>% filter(
+      !codigo_ifi %in% c(0, 1000) & !is.na(codigo_ifi)
+    ) %>% filter(
+      !codigo_ifi %in% c(1000, 300) & !is.na(codigo_ifi) 
+    ) %>% filter(
+      !codigo_ifi %in% c(1000, 500) & !is.na(codigo_ifi) 
+    ) %>% filter(
+      !codigo_ifi %in% c(1000, 10000, 10001) & !is.na(codigo_ifi) 
+    ) %>% filter(
+      !codigo_ifi %in% c(1000, 10002) & !is.na(codigo_ifi) 
+    ) %>% filter(
+      !codigo_ifi %in% c(1000, 10003) & !is.na(codigo_ifi) 
+    )
+  print("Datos transformados")
+  glimpse(temporal)
+}
 
 
 
